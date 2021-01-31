489b49db152164b63490117c0d0a0ac46e2ae06a
karan <karan.bruellau1997@gmail.com>
2021-01-31 17:07:08 +0100

    Initial commit

---
+++ src/main/java/de/leuphana/webshop/webshopclient/WebshopClientApplication.java
@@ -0,0 +1,13 @@
+package de.leuphana.webshop.webshopclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/leuphana/webshop/webshopclient/WebshopClientApplicationTests.java
@@ -0,0 +1,13 @@
+package de.leuphana.webshop.webshopclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
